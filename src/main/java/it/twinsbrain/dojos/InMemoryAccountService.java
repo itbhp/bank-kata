fd6255c56588097637dedc5b88b7dbbc5983fcc4
Paolo Sciarra <itbhps@gmail.com>
2024-01-27 22:54:46 +0100

    used a different lock, lenient on reads

--- src/main/java/it/twinsbrain/dojos/InMemoryAccountService.java
+++ src/main/java/it/twinsbrain/dojos/InMemoryAccountService.java
@@ -4,14 +4,14 @@ import it.twinsbrain.dojos.model.Balance;
 import it.twinsbrain.dojos.model.Deposit;
 import it.twinsbrain.dojos.model.Transaction;
 import it.twinsbrain.dojos.model.Withdraw;
-
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class InMemoryAccountService implements AccountService {
   private final Balance balance;
@@ -19,7 +19,7 @@ public class InMemoryAccountService implements AccountService {
   private final Display display;
 
   private final List<Transaction> transactionList = new ArrayList<>();
-  private final Lock lockOnWrite = new ReentrantLock(true);
+  private final ReadWriteLock lockOnWrite = new ReentrantReadWriteLock(true);
 
   public InMemoryAccountService(Balance balance, Time time, Display display) {
     this.balance = balance;
@@ -47,44 +47,58 @@ public class InMemoryAccountService implements AccountService {
 
   @Override
   public void printStatement() {
-    display.show("Date       || Amount || Balance");
-    var balanceReversedQueue = new LinkedList<Integer>();
-    balanceReversedQueue.add(balance.value());
-    transactionList.stream()
-        .sorted(Comparator.comparing(Transaction::time).reversed())
-        .peek(
-            m -> {
-              var prev = balanceReversedQueue.getLast();
-              var newBalance =
-                  switch (m) {
-                    case Deposit deposit -> prev - deposit.amount();
-                    case Withdraw withdraw -> prev + withdraw.amount();
-                  };
-              balanceReversedQueue.add(newBalance);
-            })
-        .forEach(
-            transaction -> {
-              var amount =
-                  switch (transaction) {
-                    case Deposit deposit -> String.valueOf(deposit.amount());
-                    case Withdraw withdraw -> "-" + withdraw.amount();
-                  };
-              var message =
-                  padRight(FORMATTER.format(transaction.time()), 11)
-                      + "|| "
-                      + padRight(amount, 7)
-                      + "|| "
-                      + balanceReversedQueue.pollFirst();
-              display.show(message);
-            });
+    atomically(
+        (balance, transactionList) -> {
+          display.show("Date       || Amount || Balance");
+          var balanceReversedQueue = new LinkedList<Integer>();
+          balanceReversedQueue.add(balance.value());
+          transactionList.stream()
+              .sorted(Comparator.comparing(Transaction::time).reversed())
+              .peek(
+                  m -> {
+                    var prev = balanceReversedQueue.getLast();
+                    var newBalance =
+                        switch (m) {
+                          case Deposit deposit -> prev - deposit.amount();
+                          case Withdraw withdraw -> prev + withdraw.amount();
+                        };
+                    balanceReversedQueue.add(newBalance);
+                  })
+              .forEach(
+                  transaction -> {
+                    var amount =
+                        switch (transaction) {
+                          case Deposit deposit -> String.valueOf(deposit.amount());
+                          case Withdraw withdraw -> "-" + withdraw.amount();
+                        };
+                    var message =
+                        padRight(FORMATTER.format(transaction.time()), 11)
+                            + "|| "
+                            + padRight(amount, 7)
+                            + "|| "
+                            + balanceReversedQueue.pollFirst();
+                    display.show(message);
+                  });
+        });
   }
 
   private void atomically(AccountUpdater action) {
+    Lock writeLock = lockOnWrite.writeLock();
     try {
-      lockOnWrite.lock();
+      writeLock.lock();
       action.update();
     } finally {
-      lockOnWrite.unlock();
+      writeLock.unlock();
+    }
+  }
+
+  private void atomically(AccountReader action) {
+    Lock readLock = lockOnWrite.readLock();
+    try {
+      readLock.lock();
+      action.read(balance, transactionList);
+    } finally {
+      readLock.unlock();
     }
   }
 
@@ -97,5 +111,10 @@ public class InMemoryAccountService implements AccountService {
   @FunctionalInterface
   private interface AccountUpdater {
     void update();
+  }
+
+  @FunctionalInterface
+  private interface AccountReader {
+    void read(Balance balance, List<Transaction> transactionList);
   }
 }
